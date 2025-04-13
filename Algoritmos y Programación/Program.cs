c4f024abeb6e37e4ba782c555f1eda925c14119e
Analista de TIC <antic@aldeasinfantiles.local>
2025-04-13 19:27:08 -0300

    Archivos de la carpeta de sharpDevelop (con algunos ejercicios) copiados a carpeta de OneDrive

---
+++ Algoritmos y Programación/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: antic
+ * Date: 7/4/2025
+ * Time: 20:54
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace Algoritmos_y_Programación
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
---
+++ Practica1/Program.cs
@@ -0,0 +1,64 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: antic
+ * Date: 7/4/2025
+ * Time: 20:57
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace Practica1
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			//Console.WriteLine("Hello World!");
+			
+			// Ejercicio 1
+			/*
+			string nombreyApellido, edad, dni;
+			Console.WriteLine("Ingrese su nombre y apellido: ");
+			nombreyApellido = Console.ReadLine();
+			Console.WriteLine("Ingrese su edad: ");
+			edad = Console.ReadLine();
+			Console.WriteLine("Ingrese su DNI: ");
+			dni = Console.ReadLine();
+			Console.WriteLine("Nombre y Apellido: "+nombreyApellido+" ("+edad+"), DNI: "+dni);
+			//Console.WriteLine($"Nombre y Apellido: {nombreyApellido} ({edad}), DNI: {dni}");
+			*/
+			
+			
+			// Ejercicio 2
+			/*
+			string nombreP1, nombreP2;
+			byte edadP1, edadP2;
+			Console.WriteLine("Calcular qué persona es mayor entre dos");
+			Console.WriteLine("Ingrese el nombre de la primera persona: ");
+			nombreP1 = Console.ReadLine();
+			Console.WriteLine("Ingrese su edad: ");
+			edadP1 = byte.Parse(Console.ReadLine());
+			Console.WriteLine("Ingrese el nombre de la segunda persona: ");
+			nombreP2 = Console.ReadLine();
+			Console.WriteLine("Ingrese su edad: ");
+			edadP2 = byte.Parse(Console.ReadLine());
+			if (edadP1 > edadP2) {
+				Console.WriteLine("El mayor es "+nombreP1);
+			} else { 
+				if (edadP1 == edadP2) {
+				Console.WriteLine(nombreP1+" y "+nombreP2+" tienen la misma edad");
+				} else {
+				Console.WriteLine("El mayor es "+nombreP2);
+				}
+			
+			}
+			*/
+			
+			
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
