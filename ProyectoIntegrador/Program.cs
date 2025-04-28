b2a307ad0fb90f92049b915c7c50a5721a1d647e
Cristian Bravo <cristiangabrielbravo92@gmail.com>
2025-04-28 16:14:17 -0300

    Se agregó una carpeta para el proyecto integrador y una imagen con el modelo UML

---
+++ ProyectoIntegrador/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Cristian Bravo
+ * Date: 28/4/2025
+ * Time: 16:09
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace ProyectoIntegrador
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
