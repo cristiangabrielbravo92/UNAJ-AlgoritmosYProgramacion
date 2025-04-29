92acc7b277c797daa9f50fde8c415c736cda4386
Cristian Bravo <cristiangabrielbravo92@gmail.com>
2025-04-28 23:33:20 -0300

    principio de ejercicio de clase alumno y clase curso

---
+++ Ejercicios de Clases/ClassAlumno.cs
@@ -0,0 +1,84 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Cristian Bravo
+ * Date: 28/4/2025
+ * Time: 23:03
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace Ejercicios_de_Clases
+{
+	/// <summary>
+	/// Description of ClassAlumno.
+	/// </summary>
+	public class ClassAlumno
+	{
+		public ClassAlumno()
+		{
+			private string nombre, apellido, legajo;
+			private int dni, nota;
+			
+			// Constructor
+			public ClassAlumno(string nom, string ape, string leg, int doc, int calificacion){
+				nombre = nom;
+				apellido = ape;
+				legajo = leg;
+				dni = doc;
+				nota = calificacion;
+			}
+			
+			public void imprimir (){
+			Console.WriteLine("Alumno: {0} {1} \nDNI: {2} | Legajo: {3}", nombre, apellido, dni,legajo);
+			}
+			
+			//propiedades
+			public string Nombre{
+			set{ nombre=value;				
+			}
+			get{ return nombre;				
+			}
+			}
+			
+			public string Apellido{
+			set{ apellido=value;				
+			}
+			get{ return apellido;				
+			}
+			}
+			
+			public int DNI{
+			set{
+				dni=value;
+			}
+			get{
+				return dni;
+			}
+			}
+			
+			public string Legajo
+			{
+			set{
+				legajo=value;
+			}
+			get{
+				return legajo;
+			}
+			}
+			
+			public int Nota{
+			set{
+				nota=value;
+			}
+			get{
+				return nota;
+			}
+			}
+
+			
+			
+			
+		}
+	}
+}
---
+++ Ejercicios de Clases/ClassCurso.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Cristian Bravo
+ * Date: 28/4/2025
+ * Time: 23:03
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace Ejercicios_de_Clases
+{
+	/// <summary>
+	/// Description of ClassCurso.
+	/// </summary>
+	public class ClassCurso
+	{
+		public ClassCurso()
+		{
+		}
+	}
+}
